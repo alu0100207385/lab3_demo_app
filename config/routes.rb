5372080d97808e32b7005ad777b17be86c44a6d3
Aaron Socas Gaspar <alu0100207385@ull.edu.es>
2014-02-27 12:05:46 +0000

    cambios1 routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -11,13 +11,13 @@ DemoApp::Application.routes.draw do
   root 'welcome#index'
 
   # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
+   get 'products/:id' => 'catalog#view'
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
-  #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
+   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
   # Example resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
+     resources :products
 
   # Example resource route with options:
   #   resources :products do
